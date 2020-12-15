3dcbc131052eb7897893b28413b1479f4663a4e1
Fahrul Zul <fahrulzul96@gmail.com>
2020-12-15 17:00:22 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Frontend Mentor - Profile card component
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+
+[Frontend Mentor](https://www.frontendmentor.io) challenges allow you to improve your skills in a real-life workflow.
+
+[The challenge](https://www.frontendmentor.io/challenges/profile-card-component-cfArpWshJ) is to build out this profile card component and get it looking as close to the design as possible.
+
+[My Solution](#)
+
+![Design preview for the Profile card component coding challenge](./design/desktop-preview.jpg)
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: ["./public/**/*.html"]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'kumbh' : ['"Kumbh sans"', 'sans-serif']
+      },
+      colors: {
+        'darkCyan' : 'hsl(185, 75%, 39%)',
+        'darkBlue' : 'hsl(229, 23%, 23%)',
+        'grayBlue' : 'hsl(227, 10%, 46%)',
+        'darkGray' : 'hsl(0, 0%, 59%)'
+      },
+      minWidth: {
+        '80' : '20rem'
+      },
+      maxWidth: {
+        '88' : '22rem',
+      },
+      width: {
+        '26' : '6.8rem',
+      },
+      height: {
+        '34' : '8.5rem',
+        '26' : '6.8rem',
+      },
+      backgroundPosition: {
+        'patternPosition' : 'right 45vw bottom 45vh, left 40vw top 40vh',
+        'patternPosition-lg' : 'right 52vw bottom 34.5vh, left 48vw top 52vh'
+      },
+      backgroundImage: theme => ({
+        'pattern' : "url('../image/bg-pattern-top.svg'), url('../image/bg-pattern-bottom.svg')"
+      }),
+      borderWidth: {
+        '6' : '6px'
+      },
+      fontSize: {
+        '2xs' : '0.65rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
